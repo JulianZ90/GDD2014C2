af388510c6d39aaa26cc3910c4894508d1a87195
hernan loiacono <hernan.loiacono@hotmail.com>
2014-10-20 17:07:43 -0300

    saque las id hardcodeadas y comentarios
    tube que cambiar generar tablas porque cambie las propiedades de
    autoindexar id en 3 tablas

--- borrar todas las tablas.sql
+++ borrar todas las tablas.sql
@@ -1,10 +1,19 @@
 /* limpiar tablas
 hay que hacerlo en el orden inverso que las creas por las fk */
+
+delete from GAME_OF_QUERYS.reserva
+delete from GAME_OF_QUERYS.estado_reserva
+delete from GAME_OF_QUERYS.habitacion
+delete from GAME_OF_QUERYS.tipo_habitacion
+delete from GAME_OF_QUERYS.cliente
+delete from GAME_OF_QUERYS.tipo_identidad
 delete from GAME_OF_QUERYS.hotel_reg
 delete from GAME_OF_QUERYS.regimen
 delete from GAME_OF_QUERYS.hotel
-delete from GAME_OF_QUERYS.cliente
-delete from GAME_OF_QUERYS.tipo_habitacion
-delete from GAME_OF_QUERYS.habitacion
-delete from GAME_OF_QUERYS.estado_reserva
-delete from GAME_OF_QUERYS.reserva
+delete from GAME_OF_QUERYS.pais
+
+
+
+
+
+
--- migracion.sql
+++ migracion.sql
@@ -1,52 +1,39 @@
-/*cargar hoteles (16) y regimenes (4) */
+/*cargar argentina en pais, hoteles (16) y regimenes (4) */
+insert into GAME_OF_QUERYS.pais (nombre) values ('Argentina')
+
+
 insert into GAME_OF_QUERYS.hotel (ciudad, calle, nro_calle, cantidad_estrella, recarga_estrella,
 								 pais_id)
 select distinct Hotel_Ciudad, Hotel_Calle, Hotel_Nro_Calle, Hotel_CantEstrella,Hotel_Recarga_Estrella,
-				'1'
+				(select id from GAME_OF_QUERYS.pais where nombre='Argentina')
 from gd_esquema.Maestra 
 
 insert into GAME_OF_QUERYS.regimen(descripcion, precio_base,estado)
 select distinct Regimen_Descripcion, Regimen_Precio,'1'
 from gd_esquema.Maestra
 
-/*cargo la tabla reg_hotel
-
-todos los hoteles tienen los 4 regimenes
-pero por las dudas hago mas verificaciones si andamos
-flojos de tiempo esto se puede mejorar*/
+/*cargo la tabla reg_hotel (4x16, todos los hoteles tienen los 4 regimenes)*/
 
 insert into GAME_OF_QUERYS.hotel_reg( hotel_id, reg_id)
 select distinct h.id, r.id
-from gd_esquema.Maestra m 
-			join GAME_OF_QUERYS.regimen r on r.descripcion=m.Regimen_Descripcion
-			join GAME_OF_QUERYS.hotel h on h.ciudad=m.Hotel_Ciudad and h.calle=m.Hotel_Calle
-							and h.nro_calle=m.Hotel_Nro_Calle
-/*order by h.hotel_id, r.reg_id opcional */
+from GAME_OF_QUERYS.hotel h, GAME_OF_QUERYS.regimen r
 
-
-
-/*cargo tipo_ident , pais y los clientes (100.740)
+/*cargo tipo_ident dni y pasaporte y los clientes (100.740)
 falta sacar horas minutos y segundos de la fecha de nacimiento*/
-insert into GAME_OF_QUERYS.pais (id, nombre) values ('1', 'Argentina')
-insert into GAME_OF_QUERYS.tipo_identidad (id, nombre) values ('1', 'DNI')
-insert into GAME_OF_QUERYS.tipo_identidad (id, nombre) values ('2', 'Pasaporte')
+
+insert into GAME_OF_QUERYS.tipo_identidad (nombre) values ('DNI')
+insert into GAME_OF_QUERYS.tipo_identidad (nombre) values ('Pasaporte')
 
 insert into GAME_OF_QUERYS.cliente (tipo_identidad_id, nro_identidad, apellido,nombre,
 					fecha_nac, mail, calle, nro_calle, piso,
-					depto, pais_origen_id,nacionalidad,permitido_ingreso)
-select distinct '2',Cliente_Pasaporte_Nro, Cliente_Apellido,
+					depto,nacionalidad,permitido_ingreso)
+select distinct (select id from GAME_OF_QUERYS.tipo_identidad where nombre='Pasaporte'),Cliente_Pasaporte_Nro, Cliente_Apellido,
 				Cliente_Nombre, Cliente_Fecha_Nac, Cliente_Mail,
 				Cliente_Dom_Calle, Cliente_Nro_Calle, Cliente_Piso,
-				Cliente_Depto, '1',Cliente_Nacionalidad,'1'
+				Cliente_Depto,Cliente_Nacionalidad,'1'
 from gd_esquema.Maestra
-/* esto es para ver los domicilios repetidos luego con un select hardcodeado
-se puede ver que es gente que vive junta 
-select distinct COUNT(*), calle, nro_calle, piso, depto
-from cliente
-group by calle, nro_calle, piso, depto
-order by 1 */
 
-/*cargo los tipos de habitacion*/
+/*cargo los tipos de habitacion (5)*/
 set identity_insert GD2C2014.GAME_OF_QUERYS.tipo_habitacion on
 insert into GAME_OF_QUERYS.tipo_habitacion (id, descripcion, porcentual)
 select distinct Habitacion_Tipo_Codigo, Habitacion_Tipo_Descripcion ,
@@ -63,14 +50,12 @@ from gd_esquema.Maestra m
 			join GAME_OF_QUERYS.hotel h on h.ciudad=m.Hotel_Ciudad and h.calle=m.Hotel_Calle
 							and h.nro_calle=m.Hotel_Nro_Calle
 							
-/* cargo estado_reserva 
-sacar los id y poner autoindexacion*/
-set identity_insert GD2C2014.GAME_OF_QUERYS.tipo_habitacion on
-insert into GAME_OF_QUERYS.estado_reserva (id,descripcion)values(1,'correcta')
-insert into GAME_OF_QUERYS.estado_reserva (id,descripcion)values(2,'modificada')
-insert into GAME_OF_QUERYS.estado_reserva (id,descripcion)values(3,'cancelada por recepcion')
-insert into GAME_OF_QUERYS.estado_reserva (id,descripcion)values(4,'cancelada por cliente')
-insert into GAME_OF_QUERYS.estado_reserva (id,descripcion)values(5,'cancelada por No-Show')
+/* cargo estado_reserva */
+insert into GAME_OF_QUERYS.estado_reserva (descripcion)values('correcta')
+insert into GAME_OF_QUERYS.estado_reserva (descripcion)values('modificada')
+insert into GAME_OF_QUERYS.estado_reserva (descripcion)values('cancelada por recepcion')
+insert into GAME_OF_QUERYS.estado_reserva (descripcion)values('cancelada por cliente')
+insert into GAME_OF_QUERYS.estado_reserva (descripcion)values('cancelada por No-Show')
 insert into GAME_OF_QUERYS.estado_reserva (descripcion)values('con ingreso')
 
 							
